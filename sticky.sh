f875993a6fe88c04920f9f52dfc7edf2a7f18e55
Jon Keam <jpkeam@gmail.com>
2022-12-09 17:09:21 -0500

    change type of service and remove balance

--- sticky.sh
+++ sticky.sh
@@ -4,4 +4,3 @@ oc project sticky-sessions
 
 oc annotate routes sticky-sessions-app router.openshift.io/cookie_name="ocpcookie"
 oc annotate routes sticky-sessions-app haproxy.router.openshift.io/disable_cookies-
-oc annotate routes sticky-sessions-app haproxy.router.openshift.io/balance-
--- unsticky.sh
+++ unsticky.sh
@@ -4,4 +4,3 @@ oc project sticky-sessions
 
 oc annotate routes sticky-sessions-app router.openshift.io/cookie_name-
 oc annotate routes sticky-sessions-app haproxy.router.openshift.io/disable_cookies='true'
-oc annotate routes sticky-sessions-app haproxy.router.openshift.io/balance='roundrobin'
